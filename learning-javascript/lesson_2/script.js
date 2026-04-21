8e84ab03bfe5c7ddcac20cfa8006c382ffc797d1
Efe Muhammed Orhan <efeorhan2005@gmail.com>
2026-04-22 00:50:02 +0300

    leseson_2 added

---
+++ learning-javascript/lesson_2/script.js
@@ -0,0 +1,21 @@
+// ---Variables---
+
+
+var a = 5000;
+var b = 5000;
+var c = 6000;
+var d = 7000;
+var kdv = 1.2;
+
+console.log(a * kdv); // item a
+console.log(b * kdv); // item b
+console.log(c * kdv); // item c
+console.log(d * kdv); // item d
+
+
+// ---Variable definition rules---
+
+// Türkçe karakter kullanılmaz.
+// Arada boşlul olmamalıdır.
+// Sayı ile başlayamaz.
+// Komut isimleri kullanılmaz.
