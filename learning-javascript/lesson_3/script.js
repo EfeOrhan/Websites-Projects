4a8dfac4113973ddbd3ba3e9736c6207c1930c01
Efe Muhammed Orhan <efeorhan2005@gmail.com>
2026-04-22 01:13:26 +0300

    lesson_3 added

---
+++ learning-javascript/lesson_3/script.js
@@ -0,0 +1,34 @@
+// Variable Types
+
+var productName = "IPhone 16"; // string
+var productPrice = "70000"; // string
+
+var productPrice_2 = 70000; // number
+
+console.log(typeof productName);
+console.log(typeof productPrice);
+console.log(typeof productPrice_2);
+
+var number_1 = 10;
+var number_2 = 20;
+
+console.log(number_1 + number_2);
+
+var name = "Efe";
+var surname = "Orhan";
+
+console.log(name + " " + surname);
+
+name = "Muhammed";
+
+console.log(name + " " + surname);
+
+var examScore = 70;
+var successful = (examScore >= 50); // boolean => True - False
+
+console.log(successful);
+
+var age;
+
+console.log(age);
+console.log(typeof age);
